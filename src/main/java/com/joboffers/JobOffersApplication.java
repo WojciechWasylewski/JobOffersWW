0dfb3299693aa46545f36ad50f3a192da24efe19
Wojciech Wasylewski <w.wasylewski@gmail.com>
2023-10-26 16:26:16 +0200

    Created packages and facades for modules

--- src/main/java/com/joboffers/JobOffersApplication.java
+++ src/main/java/com/joboffers/JobOffersApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobOffersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JobOffersApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JobOffersApplication.class, args);
+    }
 
 }
---
+++ src/test/java/com/joboffers/domain/loginandregister/LoginAndRegisterFacadeTest.java
@@ -0,0 +1,12 @@
+package com.joboffers.domain.loginandregister;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginAndRegisterFacadeTest {
+    @Test
+    public void f(){
+
+    }
+}
