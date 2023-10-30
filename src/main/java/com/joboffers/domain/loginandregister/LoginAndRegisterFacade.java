0441c21911c5cda6759152ac5bcc0bb07811f9b2
Wojciech Wasylewski <w.wasylewski@gmail.com>
2023-10-30 12:16:38 +0100

    Implemented logic for LoginAndRegisterFacade and tests.

--- src/main/java/com/joboffers/domain/loginandregister/LoginAndRegisterFacade.java
+++ src/main/java/com/joboffers/domain/loginandregister/LoginAndRegisterFacade.java
@@ -1,4 +1,27 @@
 package com.joboffers.domain.loginandregister;
 
+import com.joboffers.domain.loginandregister.dto.NewUserDto;
+import com.joboffers.domain.loginandregister.dto.RegistrationResultDto;
+import com.joboffers.domain.loginandregister.dto.UserDto;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class LoginAndRegisterFacade {
+    private static final String USER_NOT_FOUND = "User not found";
+    private final UserRepository userRepository;
+
+    public UserDto findByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .map(user -> new UserDto(user.id(), user.password(), user.username()))
+                .orElseThrow(() -> new UsernameNotFoundException(USER_NOT_FOUND));
+    }
+
+    public RegistrationResultDto register(NewUserDto newUserDto) {
+        final User user = User.builder()
+                .username(newUserDto.username())
+                .password(newUserDto.password())
+                .build();
+        User savedUser = userRepository.save(user);
+        return new RegistrationResultDto(savedUser.id(), true, savedUser.username());
+    }
 }
--- src/test/java/com/joboffers/domain/loginandregister/LoginAndRegisterFacadeTest.java
+++ src/test/java/com/joboffers/domain/loginandregister/LoginAndRegisterFacadeTest.java
@@ -1,12 +1,59 @@
 package com.joboffers.domain.loginandregister;
 
+import com.joboffers.domain.loginandregister.dto.NewUserDto;
+import com.joboffers.domain.loginandregister.dto.RegistrationResultDto;
+import com.joboffers.domain.loginandregister.dto.UserDto;
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.ThrowableAssert.catchThrowable;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class LoginAndRegisterFacadeTest {
-    @Test
-    public void f(){
+    LoginAndRegisterFacade loginFacade = new LoginAndRegisterFacade(
+            new InMemoryLoginRepository()
+    );
 
+    @Test
+    public void should_find_user_by_user_name() {
+        //given
+        NewUserDto newUserDto = new NewUserDto("username", "password");
+        RegistrationResultDto register = loginFacade.register(newUserDto);
+
+        //when
+        UserDto byUsername = loginFacade.findByUsername(register.username());
+
+        //then
+        assertThat(byUsername).isEqualTo(new UserDto(register.id(), "password", "username"));
+    }
+
+    @Test
+    public void should_throw_exception_when_user_not_found() {
+        //given
+        String username = "someUser";
+
+        //when
+        Throwable thrown = catchThrowable(() -> loginFacade.findByUsername(username));
+
+        //then
+        AssertionsForClassTypes.assertThat(thrown)
+                .isInstanceOf(UsernameNotFoundException.class)
+                .hasMessage("User not found");
+    }
+
+    @Test
+    public void should_register_user() {
+        //given
+        NewUserDto newUserDto = new NewUserDto("username", "password");
+
+        //when
+        RegistrationResultDto register = loginFacade.register(newUserDto);
+
+        //then
+        assertAll(
+                () -> assertThat(register.created()).isTrue(),
+                () -> assertThat(register.username()).isEqualTo("username")
+        );
     }
 }
