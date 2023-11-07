6d893d21d9597c329e3e47a5104ef27ca59a9587
Wojciech Wasylewski <w.wasylewski@gmail.com>
2023-11-07 12:07:26 +0100

    added RestController GET /offers and test

--- src/integration/java/com/joboffers/features/TypicalScenarioUserWantsToSeeOffersIntegrationTest.java
+++ src/integration/java/com/joboffers/features/TypicalScenarioUserWantsToSeeOffersIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.joboffers.features;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.joboffers.BaseIntegrationTests;
 import com.joboffers.SampleJobOfferResponse;
@@ -8,33 +9,57 @@ import com.joboffers.infrastructure.offer.scheduler.HttpOffersScheduler;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class TypicalScenarioUserWantsToSeeOffersIntegrationTest extends BaseIntegrationTests implements SampleJobOfferResponse {
     @Autowired
     HttpOffersScheduler httpOffersScheduler;
     @Test
-    public void user_want_to_see_offers_but_have_to_be_logged_in_and_external_server_should_have_some_offers() {
-//    Step 1: external http server dont have any job offers.
+    public void user_want_to_see_offers_but_have_to_be_logged_in_and_external_server_should_have_some_offers() throws Exception {
+//    Step 1: external http server don't have any job offers.
         wireMockServer.stubFor(WireMock.get("/offers")
                 .willReturn(WireMock.aResponse()
                         .withStatus(HttpStatus.OK.value())
                         .withHeader("Content-Type", "application/json")
                         .withBody(bodyWithZeroOffersJson())));
+
+
 //    Step 2: scheduler run 1st time and made GET to external server and download zero offers to database.
         // given && when
         List<OfferResponseDto> newOffers = httpOffersScheduler.fetchAllOffersAndSaveAllIfNotExists();
         // then
         assertThat(newOffers).isEmpty();
 
+
 //    Step 3: user which is not registered yet tried to get JWT token by requesting POST with: username=someUser, password=somePassword and system returns UNAUTHORIZED (401).
 //    Step 4: user made GET /offers with no JWT token znd system returned UNAUTHORIZED (401).
 //    Step 5: user made POST /register with: username=someUser, password=somePassword and system registered and returned status OK(200).
 //    Step 6: user tried get JWT token by requesting POST /token with username=someUser, password=somePassword and system returned OK(200) and jwttoken=AAAA.BBBB.CCC
+
+
 //    Step 7: user made GET /offers with header ”Authorization: Bearer AAAA.BBBB.CCC” and system returned OK(200) with no offers (because after first scheduler run downloaded 0 offers).
+        //given
+        String offersUrl = "/offers";
+        //when
+        ResultActions perform = mockMvc.perform(get(offersUrl)
+                .contentType(MediaType.APPLICATION_JSON)
+        );
+        // then
+        MvcResult result = perform.andExpect(status().isOk()).andReturn();
+        String json = result.getResponse().getContentAsString();
+        List<OfferResponseDto> offers = objectMapper.readValue(json, new TypeReference<>() {
+        });
+        assertThat(offers).isEmpty();
+
+
 //    Step 8: external HTTP server have 2 new offers.
 //    Step 9: scheduler run 2nd time and made GET to external server and 2 offers with id:1000 and id:2000 are added to database.
 //    Step 10: user made GET /offers with header ”Authorization: Bearer AAAA.BBBB.CCC” and system returned OK(200) with 2 offers with id:1000 and id:2000
